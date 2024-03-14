d5f6d5cbb2cd9a913b60db7ddb9b608520781119
KAM-2006 <dinaoftheday@abv.bg>
2024-03-14 15:53:01 +0200

    redoing form1 and little part of tablesform desings

---
+++ WinFormsFilmsCatalog/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WinFormsFilmsCatalog
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnOtherOperations = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Rockwell", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lblText.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblText.Location = new System.Drawing.Point(12, 33);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(598, 79);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "Каталог за филми";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.Color.White;
+            this.btnInsert.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnInsert.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnInsert.Location = new System.Drawing.Point(169, 162);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(284, 91);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "Вмъкни информация в базата";
+            this.btnInsert.UseVisualStyleBackColor = false;
+            // 
+            // btnShow
+            // 
+            this.btnShow.BackColor = System.Drawing.Color.White;
+            this.btnShow.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnShow.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnShow.Location = new System.Drawing.Point(169, 287);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(284, 91);
+            this.btnShow.TabIndex = 2;
+            this.btnShow.Text = "Покажи таблиците: Филми, Актьори, Жанр";
+            this.btnShow.UseVisualStyleBackColor = false;
+            // 
+            // btnOtherOperations
+            // 
+            this.btnOtherOperations.BackColor = System.Drawing.Color.White;
+            this.btnOtherOperations.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnOtherOperations.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnOtherOperations.Location = new System.Drawing.Point(169, 415);
+            this.btnOtherOperations.Name = "btnOtherOperations";
+            this.btnOtherOperations.Size = new System.Drawing.Size(284, 91);
+            this.btnOtherOperations.TabIndex = 3;
+            this.btnOtherOperations.Text = "Други функционалности";
+            this.btnOtherOperations.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(633, 569);
+            this.Controls.Add(this.btnOtherOperations);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.lblText);
+            this.Name = "Form1";
+            this.Text = "Начална форма";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblText;
+        private Button btnInsert;
+        private Button btnShow;
+        private Button btnOtherOperations;
+    }
+}
---
+++ WinFormsFilmsCatalog/TablesForm.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace WinFormsFilmsCatalog
+{
+    partial class TablesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFilmF = new System.Windows.Forms.Label();
+            this.lblFilmI = new System.Windows.Forms.Label();
+            this.lblFilmL = new System.Windows.Forms.Label();
+            this.lblFilmM = new System.Windows.Forms.Label();
+            this.lblFilmInfo = new System.Windows.Forms.Label();
+            this.lblActorA = new System.Windows.Forms.Label();
+            this.lblActorC = new System.Windows.Forms.Label();
+            this.lblActorT = new System.Windows.Forms.Label();
+            this.lblActor = new System.Windows.Forms.Label();
+            this.lblActorO = new System.Windows.Forms.Label();
+            this.lblActorR = new System.Windows.Forms.Label();
+            this.lblInfoA = new System.Windows.Forms.Label();
+            this.lblGenreG = new System.Windows.Forms.Label();
+            this.lblGenreE = new System.Windows.Forms.Label();
+            this.lblGenreN = new System.Windows.Forms.Label();
+            this.lblGenreRE = new System.Windows.Forms.Label();
+            this.lblInfoG = new System.Windows.Forms.Label();
+            this.lblTitleF = new System.Windows.Forms.Label();
+            this.lblDirectorF = new System.Windows.Forms.Label();
+            this.lblDateOfReleasеF = new System.Windows.Forms.Label();
+            this.lblRatingF = new System.Windows.Forms.Label();
+            this.lblGenreIdF = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblFilmF
+            // 
+            this.lblFilmF.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.lblFilmF.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblFilmF.Location = new System.Drawing.Point(12, 31);
+            this.lblFilmF.Name = "lblFilmF";
+            this.lblFilmF.Size = new System.Drawing.Size(42, 40);
+            this.lblFilmF.TabIndex = 0;
+            this.lblFilmF.Text = "Ф";
+            this.lblFilmF.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFilmI
+            // 
+            this.lblFilmI.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.lblFilmI.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblFilmI.Location = new System.Drawing.Point(12, 71);
+            this.lblFilmI.Name = "lblFilmI";
+            this.lblFilmI.Size = new System.Drawing.Size(42, 40);
+            this.lblFilmI.TabIndex = 1;
+            this.lblFilmI.Text = "И";
+            this.lblFilmI.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFilmL
+            // 
+            this.lblFilmL.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.lblFilmL.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblFilmL.Location = new System.Drawing.Point(12, 111);
+            this.lblFilmL.Name = "lblFilmL";
+            this.lblFilmL.Size = new System.Drawing.Size(42, 40);
+            this.lblFilmL.TabIndex = 2;
+            this.lblFilmL.Text = "Л";
+            this.lblFilmL.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFilmM
+            // 
+            this.lblFilmM.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.lblFilmM.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblFilmM.Location = new System.Drawing.Point(12, 151);
+            this.lblFilmM.Name = "lblFilmM";
+            this.lblFilmM.Size = new System.Drawing.Size(42, 40);
+            this.lblFilmM.TabIndex = 3;
+            this.lblFilmM.Text = "М";
+            this.lblFilmM.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFilmInfo
+            // 
+            this.lblFilmInfo.AutoSize = true;
+            this.lblFilmInfo.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblFilmInfo.Location = new System.Drawing.Point(55, 9);
+            this.lblFilmInfo.Name = "lblFilmInfo";
+            this.lblFilmInfo.Size = new System.Drawing.Size(394, 25);
+            this.lblFilmInfo.TabIndex = 4;
+            this.lblFilmInfo.Text = "Въведете информация за таблицата Филми:";
+            // 
+            // lblActorA
+            // 
+            this.lblActorA.BackColor = System.Drawing.Color.Turquoise;
+            this.lblActorA.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblActorA.Location = new System.Drawing.Point(12, 240);
+            this.lblActorA.Name = "lblActorA";
+            this.lblActorA.Size = new System.Drawing.Size(42, 40);
+            this.lblActorA.TabIndex = 5;
+            this.lblActorA.Text = "А";
+            this.lblActorA.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblActorC
+            // 
+            this.lblActorC.BackColor = System.Drawing.Color.Turquoise;
+            this.lblActorC.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblActorC.Location = new System.Drawing.Point(12, 280);
+            this.lblActorC.Name = "lblActorC";
+            this.lblActorC.Size = new System.Drawing.Size(42, 40);
+            this.lblActorC.TabIndex = 6;
+            this.lblActorC.Text = "К";
+            this.lblActorC.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblActorT
+            // 
+            this.lblActorT.BackColor = System.Drawing.Color.Turquoise;
+            this.lblActorT.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblActorT.Location = new System.Drawing.Point(12, 320);
+            this.lblActorT.Name = "lblActorT";
+            this.lblActorT.Size = new System.Drawing.Size(42, 40);
+            this.lblActorT.TabIndex = 7;
+            this.lblActorT.Text = "Т";
+            this.lblActorT.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblActor
+            // 
+            this.lblActor.BackColor = System.Drawing.Color.Turquoise;
+            this.lblActor.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblActor.Location = new System.Drawing.Point(12, 360);
+            this.lblActor.Name = "lblActor";
+            this.lblActor.Size = new System.Drawing.Size(42, 40);
+            this.lblActor.TabIndex = 8;
+            this.lblActor.Text = "Ь";
+            this.lblActor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblActorO
+            // 
+            this.lblActorO.BackColor = System.Drawing.Color.Turquoise;
+            this.lblActorO.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblActorO.Location = new System.Drawing.Point(12, 400);
+            this.lblActorO.Name = "lblActorO";
+            this.lblActorO.Size = new System.Drawing.Size(42, 40);
+            this.lblActorO.TabIndex = 9;
+            this.lblActorO.Text = "О";
+            this.lblActorO.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblActorR
+            // 
+            this.lblActorR.BackColor = System.Drawing.Color.Turquoise;
+            this.lblActorR.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblActorR.Location = new System.Drawing.Point(12, 440);
+            this.lblActorR.Name = "lblActorR";
+            this.lblActorR.Size = new System.Drawing.Size(42, 40);
+            this.lblActorR.TabIndex = 10;
+            this.lblActorR.Text = "Р";
+            this.lblActorR.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblInfoA
+            // 
+            this.lblInfoA.AutoSize = true;
+            this.lblInfoA.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblInfoA.Location = new System.Drawing.Point(55, 212);
+            this.lblInfoA.Name = "lblInfoA";
+            this.lblInfoA.Size = new System.Drawing.Size(407, 25);
+            this.lblInfoA.TabIndex = 11;
+            this.lblInfoA.Text = "Въведете информация за таблицата Актьори:";
+            // 
+            // lblGenreG
+            // 
+            this.lblGenreG.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.lblGenreG.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblGenreG.Location = new System.Drawing.Point(12, 529);
+            this.lblGenreG.Name = "lblGenreG";
+            this.lblGenreG.Size = new System.Drawing.Size(42, 40);
+            this.lblGenreG.TabIndex = 12;
+            this.lblGenreG.Text = "Ж";
+            this.lblGenreG.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGenreE
+            // 
+            this.lblGenreE.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.lblGenreE.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblGenreE.Location = new System.Drawing.Point(12, 569);
+            this.lblGenreE.Name = "lblGenreE";
+            this.lblGenreE.Size = new System.Drawing.Size(42, 40);
+            this.lblGenreE.TabIndex = 13;
+            this.lblGenreE.Text = "А";
+            this.lblGenreE.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGenreN
+            // 
+            this.lblGenreN.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.lblGenreN.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblGenreN.Location = new System.Drawing.Point(12, 609);
+            this.lblGenreN.Name = "lblGenreN";
+            this.lblGenreN.Size = new System.Drawing.Size(42, 40);
+            this.lblGenreN.TabIndex = 14;
+            this.lblGenreN.Text = "Н";
+            this.lblGenreN.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblGenreRE
+            // 
+            this.lblGenreRE.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.lblGenreRE.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblGenreRE.Location = new System.Drawing.Point(12, 649);
+            this.lblGenreRE.Name = "lblGenreRE";
+            this.lblGenreRE.Size = new System.Drawing.Size(42, 40);
+            this.lblGenreRE.TabIndex = 15;
+            this.lblGenreRE.Text = "Р";
+            this.lblGenreRE.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblInfoG
+            // 
+            this.lblInfoG.AutoSize = true;
+            this.lblInfoG.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblInfoG.Location = new System.Drawing.Point(55, 504);
+            this.lblInfoG.Name = "lblInfoG";
+            this.lblInfoG.Size = new System.Drawing.Size(383, 25);
+            this.lblInfoG.TabIndex = 16;
+            this.lblInfoG.Text = "Въведете информация за таблицата Жанр:";
+            // 
+            // lblTitleF
+            // 
+            this.lblTitleF.AutoSize = true;
+            this.lblTitleF.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitleF.Location = new System.Drawing.Point(60, 43);
+            this.lblTitleF.Name = "lblTitleF";
+            this.lblTitleF.Size = new System.Drawing.Size(94, 25);
+            this.lblTitleF.TabIndex = 17;
+            this.lblTitleF.Text = "Заглавие:";
+            // 
+            // lblDirectorF
+            // 
+            this.lblDirectorF.AutoSize = true;
+            this.lblDirectorF.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDirectorF.Location = new System.Drawing.Point(60, 71);
+            this.lblDirectorF.Name = "lblDirectorF";
+            this.lblDirectorF.Size = new System.Drawing.Size(103, 25);
+            this.lblDirectorF.TabIndex = 18;
+            this.lblDirectorF.Text = "Режисьор:";
+            // 
+            // lblDateOfReleasеF
+            // 
+            this.lblDateOfReleasеF.AutoSize = true;
+            this.lblDateOfReleasеF.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblDateOfReleasеF.Location = new System.Drawing.Point(60, 96);
+            this.lblDateOfReleasеF.Name = "lblDateOfReleasеF";
+            this.lblDateOfReleasеF.Size = new System.Drawing.Size(168, 25);
+            this.lblDateOfReleasеF.TabIndex = 19;
+            this.lblDateOfReleasеF.Text = "Дата на издаване:";
+            // 
+            // lblRatingF
+            // 
+            this.lblRatingF.AutoSize = true;
+            this.lblRatingF.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblRatingF.Location = new System.Drawing.Point(60, 126);
+            this.lblRatingF.Name = "lblRatingF";
+            this.lblRatingF.Size = new System.Drawing.Size(85, 25);
+            this.lblRatingF.TabIndex = 20;
+            this.lblRatingF.Text = "Рейтинг:";
+            // 
+            // lblGenreIdF
+            // 
+            this.lblGenreIdF.AutoSize = true;
+            this.lblGenreIdF.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblGenreIdF.Location = new System.Drawing.Point(60, 151);
+            this.lblGenreIdF.Name = "lblGenreIdF";
+            this.lblGenreIdF.Size = new System.Drawing.Size(96, 25);
+            this.lblGenreIdF.TabIndex = 21;
+            this.lblGenreIdF.Text = "Жанр ИД:";
+            // 
+            // TablesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1305, 716);
+            this.Controls.Add(this.lblGenreIdF);
+            this.Controls.Add(this.lblRatingF);
+            this.Controls.Add(this.lblDateOfReleasеF);
+            this.Controls.Add(this.lblDirectorF);
+            this.Controls.Add(this.lblTitleF);
+            this.Controls.Add(this.lblInfoG);
+            this.Controls.Add(this.lblGenreRE);
+            this.Controls.Add(this.lblGenreN);
+            this.Controls.Add(this.lblGenreE);
+            this.Controls.Add(this.lblGenreG);
+            this.Controls.Add(this.lblInfoA);
+            this.Controls.Add(this.lblActorR);
+            this.Controls.Add(this.lblActorO);
+            this.Controls.Add(this.lblActor);
+            this.Controls.Add(this.lblActorT);
+            this.Controls.Add(this.lblActorC);
+            this.Controls.Add(this.lblActorA);
+            this.Controls.Add(this.lblFilmInfo);
+            this.Controls.Add(this.lblFilmM);
+            this.Controls.Add(this.lblFilmL);
+            this.Controls.Add(this.lblFilmI);
+            this.Controls.Add(this.lblFilmF);
+            this.Name = "TablesForm";
+            this.Text = "TablesForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblFilmF;
+        private Label lblFilmI;
+        private Label lblFilmL;
+        private Label lblFilmM;
+        private Label lblFilmInfo;
+        private Label lblActorA;
+        private Label lblActorC;
+        private Label lblActorT;
+        private Label lblActor;
+        private Label lblActorO;
+        private Label lblActorR;
+        private Label lblInfoA;
+        private Label lblGenreG;
+        private Label lblGenreE;
+        private Label lblGenreN;
+        private Label lblGenreRE;
+        private Label lblInfoG;
+        private Label lblTitleF;
+        private Label lblDirectorF;
+        private Label lblDateOfReleasеF;
+        private Label lblRatingF;
+        private Label lblGenreIdF;
+    }
+}
